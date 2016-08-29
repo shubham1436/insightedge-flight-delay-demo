12b1779b8f1934f92cc8666d46123834c2770e24
Danylo Hurin <danylo@gigaspaces.com>
2016-08-29 16:30:22 +0300

    Update article and second job

--- article/article.md
+++ article/article.md
@@ -34,7 +34,6 @@ Job consists of next simple steps:
 
 1. Load data and split it on training and testing part and save testing part for second job usage. We will use the same [data set](https://github.com/InsightEdge/insightedge-python-demo/blob/master/data/flights_jan_2014.csv) as in Carol McDonald's blog. 
 ```python
-
 flight_data_file = ...
 sc = SparkContext(appName="Flight prediction model training")
 
@@ -42,12 +41,10 @@ text_rdd = sc.textFile(flight_data_file)
 splits = text_rdd.randomSplit([0.7, 0.3])
 (training_rdd, test_rdd) = (splits[0], splits[1])
 test_rdd.coalesce(1, True).saveAsTextFile(...)
-
 ```
 
 2. During second job we will convert flight data into LabeledPoint. For this we will store integer representations of origin, destination and carrier in the data grid.
 ```python
-
 all_flights_rdd = text_rdd.map(lambda r: Utils.parse_flight(r))
 
 carrier_mapping = dict(all_flights_rdd.map(lambda flight: flight.carrier).distinct().zipWithIndex().collect())
@@ -58,27 +55,51 @@ sqlc = SQLContext(sc)
 save_mapping(carrier_mapping, DF_SUFFIX + ".CarrierMap", sqlc)
 save_mapping(origin_mapping, DF_SUFFIX + ".OriginMap", sqlc)
 save_mapping(destination_mapping, DF_SUFFIX + ".DestinationMap", sqlc)
-
 ```
 
 3. Train model and save it to the grid
 ```python
-
 model = DecisionTree.trainClassifier(training_data, classes_count, categorical_features_info,
                                          impurity, max_depth, max_bins)
 Utils.save_model_to_grid(model, sc)
-
 ```
 
 
 #### 'Flight delay prediction' Spark job
-Second Spark job will load model from the grid, read data from stream and use the model for prediction. Prediction results are stored in the grid alongside with flight data.
-Load model and helper data from grid
-Simple Kafka feeder
-Open Kafka stream + use function for each rdd
-Parse and prepare data  + predict
-Save parsed data + prediction to grod
 
+Second Spark job will load model and mappings from the grid, read data from stream and use the model for prediction. Prediction results are stored in the grid alongside with flight data.
+Let's see main steps:
+
+1. Load models and mapping form data grid
+```python
+sc = SparkContext(appName="Flight delay prediction job")
+model = DecisionTreeModel(Utils.load_model_from_grid(sc))
+
+sqlc = SQLContext(sc)
+carrier_mapping = load_mapping(DF_SUFFIX + ".CarrierMap", sqlc)
+origin_mapping = load_mapping(DF_SUFFIX + ".OriginMap", sqlc)
+destination_mapping = load_mapping(DF_SUFFIX + ".DestinationMap", sqlc)
+```
+
+2. Open Kafka stream and parse lines with flight data
+```python
+ssc = StreamingContext(sc, 3)
+kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
+lines = kvs.map(lambda x: x[1])
+lines.foreachRDD(lambda rdd: predict_and_save(rdd, model, carrier_mapping, origin_mapping, destination_mapping))
+```
+
+3. Final step is to parse bunch of lines(rdd), do a prediction and save it to the data grid 
+```python
+parsed_flights = rdd.map(Utils.parse_flight)
+labeled_points = parsed_flights.map(lambda flight: Utils.create_labeled_point(flight, carrier_mapping, origin_mapping, destination_mapping))
+
+predictions = model.predict(labeled_points.map(lambda x: x.features))
+labels_and_predictions = labeled_points.map(lambda lp: lp.label).zip(predictions).zip(parsed_flights)
+
+df = sqlc.createDataFrame(labels_and_predictions)
+df.write.format(IE_FORMAT).mode("append").save(DF_SUFFIX + ".FlightWithPrediction")
+```
 
 ### Showing results
 
--- python/pydemo/flight_prediction_job.py
+++ python/pydemo/flight_prediction_job.py
@@ -49,7 +49,7 @@ FROM FlightWithPrediction
 '''
 
 
-def get_mapping(mapping_name, sqlc):
+def load_mapping(mapping_name, sqlc):
     df = sqlc.read.format(IE_FORMAT).option("collection", mapping_name).load()
     return dict(df.map(lambda row: (row.key, row.integer_value)).collect())
 
@@ -57,39 +57,49 @@ def get_mapping(mapping_name, sqlc):
 def predict_and_save(rdd):
     if not rdd.isEmpty():
         parsed_flights = rdd.map(Utils.parse_flight)
-        data = parsed_flights.map(lambda flight: Utils.create_labeled_point(flight, carrier_map, origin_map, destination_map))
-        # data.collect()
+        labeled_points = parsed_flights.map(lambda flight: Utils.create_labeled_point(flight, carrier_mapping, origin_mapping, destination_mapping))
 
-        predictions = model.predict(data.map(lambda x: x.features))
-        labelsAndPredictions = data.map(lambda lp: lp.label).zip(predictions).zip(parsed_flights) \
-            .map(lambda t: Row(actual=t[0][0], prediction=t[0][1],
-                               day_of_month=t[1].day_of_month, day_of_week=t[1].day_of_week, carrier=t[1].carrier, tail_number=t[1].tail_number,
-                               flight_number=t[1].flight_number, origin_id=t[1].origin_id, origin=t[1].origin, destination_id=t[1].destination_id,
-                               destination=t[1].destination, scheduled_departure_time=t[1].scheduled_departure_time, actual_departure_time=t[1].actual_departure_time,
-                               departure_delay_minutes=t[1].departure_delay_minutes,
-                               scheduled_arrival_time=t[1].scheduled_arrival_time, actual_arrival_time=t[1].actual_arrival_time, arrival_delay_minutes=t[1].arrival_delay_minutes,
-                               crs_elapsed_flight_minutes=t[1].crs_elapsed_flight_minutes,
-                               distance=t[1].distance))
+        predictions = model.predict(labeled_points.map(lambda x: x.features))
+        labels_and_predictions = labeled_points.map(lambda lp: lp.label).zip(predictions).zip(parsed_flights).map(to_row())
 
-        df = sqlc.createDataFrame(labelsAndPredictions)
+        df = sqlc.createDataFrame(labels_and_predictions)
         df.write.format(IE_FORMAT).mode("append").save(DF_SUFFIX + ".FlightWithPrediction")
-        df.show()
+
+
+def to_row():
+    return lambda t: Row(actual=t[0][0],
+                         prediction=t[0][1],
+                         day_of_month=t[1].day_of_month,
+                         day_of_week=t[1].day_of_week,
+                         carrier=t[1].carrier,
+                         tail_number=t[1].tail_number,
+                         flight_number=t[1].flight_number,
+                         origin_id=t[1].origin_id, origin=t[1].origin,
+                         destination_id=t[1].destination_id,
+                         destination=t[1].destination,
+                         scheduled_departure_time=t[1].scheduled_departure_time,
+                         actual_departure_time=t[1].actual_departure_time,
+                         departure_delay_minutes=t[1].departure_delay_minutes,
+                         scheduled_arrival_time=t[1].scheduled_arrival_time,
+                         actual_arrival_time=t[1].actual_arrival_time,
+                         arrival_delay_minutes=t[1].arrival_delay_minutes,
+                         crs_elapsed_flight_minutes=t[1].crs_elapsed_flight_minutes,
+                         distance=t[1].distance)
 
 
 if __name__ == "__main__":
-    sc = SparkContext(appName="Insightedge Python API example: use job")
+    sc = SparkContext(appName="Flight delay prediction job")
     ssc = StreamingContext(sc, 3)
     sqlc = SQLContext(sc)
 
     zkQuorum = "localhost:2181"
     topic = "python-blog"
 
-    loaded_model = Utils.load_model_from_grid(sc)
-    model = DecisionTreeModel(loaded_model)
+    model = DecisionTreeModel(Utils.load_model_from_grid(sc))
 
-    carrier_map = get_mapping(DF_SUFFIX + ".CarrierMap", sqlc)
-    origin_map = get_mapping(DF_SUFFIX + ".OriginMap", sqlc)
-    destination_map = get_mapping(DF_SUFFIX + ".DestinationMap", sqlc)
+    carrier_mapping = load_mapping(DF_SUFFIX + ".CarrierMap", sqlc)
+    origin_mapping = load_mapping(DF_SUFFIX + ".OriginMap", sqlc)
+    destination_mapping = load_mapping(DF_SUFFIX + ".DestinationMap", sqlc)
 
     kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
     lines = kvs.map(lambda x: x[1])
@@ -97,19 +107,3 @@ if __name__ == "__main__":
 
     ssc.start()
     ssc.awaitTermination()
-
-# def predict_and_save_simple(rdd):
-#     if not rdd.isEmpty():
-#         data = rdd.map(lambda line: line.split(',')).map(lambda arr: LabeledPoint(arr[0],
-#                                                                                 Vectors.dense(arr[1], arr[2], arr[3],
-#                                                                                               arr[4], arr[5], arr[6],
-#                                                                                               arr[7], arr[8])))
-#         #data.collect()
-#
-#         predictions = model.predict(data.map(lambda x: x.features))
-#         labelsAndPredictions = data.map(lambda lp: lp.label).zip(predictions) \
-#             .map(lambda t: Row(real=t[0], prediction=t[1]))
-#
-#         df = sqlc.createDataFrame(labelsAndPredictions)
-#         df.write.format(IE_FORMAT).mode("append").save("flight_tree_real_and_predictions")
-#         df.show()
