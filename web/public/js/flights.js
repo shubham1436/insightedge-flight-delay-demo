5a3de180bacf6b8ed268b011226db81a5f16a300
Danylo Hurin <danylo@gigaspaces.com>
2016-09-08 18:38:20 +0300

    UI improve

--- web/public/js/flights.js
+++ web/public/js/flights.js
@@ -7,7 +7,6 @@ $(function () {
     $.demo.streamedCount = 0;
 });
 
-
 if (window.console) {
   console.log("Welcome to your Play application's JavaScript!");
 }
@@ -20,18 +19,55 @@ function getFlights() {
             var tableToInsert = ""
             if (flight.streamed == "1") {
                 $.demo.streamedCount += 1;
-                tableToInsert = "streamedFlightsTable"
                 if (flight.rowId > $.demo.streamedLastRowId) {
                     $.demo.streamedLastRowId = flight.rowId
                 }
+                var row = [];
+                row.push('<tr>')
+                row.push('<td>');     row.push(flight.rowId);                   row.push('</td>');
+                row.push('<td>Jan '); row.push(flight.dayOfMonth);              row.push('</td>');
+                row.push('<td>');     row.push(flight.carrier);                 row.push('</td>');
+                row.push('<td>');     row.push(flight.origin);                  row.push('</td>');
+                row.push('<td>');     row.push(flight.destination);             row.push('</td>');
+                row.push('<td>');     row.push(flight.scheduledDepartureTime);  row.push('</td>');
+                row.push('<td>');     row.push(flight.scheduledArrivalTime);    row.push('</td>');
+                row.push('<td>');     row.push(flight.departureDelayMinutes);   row.push('</td>');
+                if ((flight.prediction == "0" && flight.departureDelayMinutes <= 40) ||
+                    (flight.prediction == "1" && flight.departureDelayMinutes > 40)) {
+                    row.push('<td class="success">Correct');
+                } else {
+                    row.push('<td class="warning">Incorrect');
+                }
+                row.push('</td>');
+                row.push('</tr>')
+                var combinedRow = row.join("");
+                $('#streamedFlightsTable tr:last').after(combinedRow);
+                //$('#streamedFlightsTable tr:last').after('<tr><td>' + flight.rowId + '</td><td>' + flight.streamed + '</td><td>' + flight.origin + '</td><td>' + flight.destination + '</td><td>' + flight.departureDelayMinutes + '</td><td>' + flight.prediction + '</td></tr>');
             } else {
                 $.demo.submittedCount += 1
-                tableToInsert = "submittedFlightsTable"
                 if (flight.rowId > $.demo.submittedLastRowId) {
                     $.demo.submittedLastRowId = flight.rowId
                 }
+                var row = [];
+                row.push('<tr>')
+                row.push('<td>'); row.push(flight.rowId);                   row.push('</td>');
+                row.push('<td>'); row.push(flight.dayOfMonth);              row.push('</td>');
+                row.push('<td>'); row.push(flight.carrier);                 row.push('</td>');
+                row.push('<td>'); row.push(flight.origin);                  row.push('</td>');
+                row.push('<td>'); row.push(flight.destination);             row.push('</td>');
+                row.push('<td>'); row.push(flight.scheduledDepartureTime);  row.push('</td>');
+                row.push('<td>'); row.push(flight.scheduledArrivalTime);    row.push('</td>');
+                if (flight.prediction == "0") {
+                    row.push('<td class="success">No Delay');
+                } else {
+                    row.push('<td class="warning">Delay');
+                }
+                row.push('</td>');
+                row.push('</tr>')
+                var combinedRow = row.join("");
+                $('#submittedFlightsTable tr:last').after(combinedRow);
+                //$('#submittedFlightsTable tr:last').after('<tr><td>' + flight.rowId + '</td><td>Jan ' + flight.dayOfMonth + '</td><td>' + flight.carrier + '</td><td>' + flight.origin + '</td><td>' + flight.destination + '</td><td>' + flight.departureDelayMinutes + '</td><td>' + flight.prediction + '</td></tr>');
             }
-            $('#'+tableToInsert+' tr:last').after('<tr><td>' + flight.rowId + '</td><td>' + flight.streamed + '</td><td>' + flight.origin + '</td><td>' + flight.destination + '</td><td>' + flight.departureDelayMinutes + '</td><td>' + flight.prediction + '</td></tr>');
             $('#submittedCount').text($.demo.submittedCount)
             $('#streamedCount').text($.demo.streamedCount)
         });
@@ -39,7 +75,7 @@ function getFlights() {
         console.log("Last submitted: " + $.demo.submittedLastRowId);
     })
 
-    setTimeout(function() {getFlights();}, 2000);
+    setTimeout(function() {getFlights();}, $.demo.refreshRate);
 }
 
 function submitFlight() {
